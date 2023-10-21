a3d9c3b960a3b66ddc2b103f1c6d0eca4ab1d8c3
quan4259 <86346279+quan4259@users.noreply.github.com>
2023-10-21 11:39:28 +0700

    th

--- Controller/admin/Crud/ThuongHieu/NewBrand.php
+++ Controller/admin/Crud/ThuongHieu/NewBrand.php
@@ -1,64 +1,4 @@
 <?php
-
-// include __DIR__ . '../../../../../Modal/Database.php';
-
-// $db = new Database;
-// $db->connect();
-// $conect = $db->conn;
-
-// // Xử lý dữ liệu ở đây (ví dụ: lấy dữ liệu từ cơ sở dữ liệu)
-// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//     // Lấy dữ liệu từ form
-//     $SoThuTu = $_POST['SoThuTu'];
-//     $TenGoi = $_POST['TenGoi'];
-
-//     $uploadDir = 'uploads/'; // Thư mục lưu trữ tệp ảnh trên máy chủ
-//     $file = $_FILES['duong_dan_tai_lieu'];
-
-//      // Kiểm tra xem tệp ảnh đã được tải lên thành công
-//      if ($file['error'][0] === UPLOAD_ERR_OK) {
-//         // Tạo tên tệp mới dựa trên thời gian
-//         $fileName = time() . '_' . $file['name'][0];
-//         $filePath = $uploadDir . $fileName;
-
-//         // Di chuyển tệp ảnh vào thư mục lưu trữ
-//         if (move_uploaded_file($file['tmp_name'][0], $filePath)) {
-//             // Thêm thông tin và đường dẫn tệp ảnh vào CSDL
-//             $sql = "INSERT INTO ten_bang (SoThuTu, TenGoi, DuongDanAnh) VALUES (?, ?, ?)";
-//             $stmt = $conn->prepare($sql);
-//             $stmt->bind_param("sss", $soThuTu, $tenGoi, $filePath);
-
-//             if ($stmt->execute()) {
-//                 $response = ["status" => true];
-//             } else {
-//                 $response = ["status" => false];
-//             }
-
-//             $stmt->close();
-//         } else {
-//             $response = ["status" => false];
-//         }
-//     } else {
-//         $response = ["status" => false];
-//     }
-
-//     // $sql = "INSERT INTO tbl_thuonghieu (Stt, TenGoi) VALUES ('$SoThuTu', '$TenGoi')";
-//     // $result = $conect->query($sql);
-
-//     // $data = array(
-//     //     'SoThuTu' =>  $SoThuTu,
-//     //     'TenGoi' => $TenGoi,
-//     //     'status' => true,
-//     // );
-
-//     // Trả về dữ liệu dưới dạng JSON
-//     header('Content-Type: application/json');
-//     echo json_encode($data);
-// }
-
-// $db->closeDatabase();
-
-
 include __DIR__ . '../../../../../Modal/Database.php';
 
 $db = new Database;
@@ -72,7 +12,6 @@ if (!$conn) {
     exit();
 }
 
-// Xử lý dữ liệu ở đây (ví dụ: lấy dữ liệu từ cơ sở dữ liệu)
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Lấy dữ liệu từ form
     $SoThuTu = $_POST['SoThuTu'];
@@ -81,30 +20,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $uploadDir = '../../../../UpLoad/ThuongHieu/'; // Thư mục lưu trữ tệp ảnh trên máy chủ
     $file = $_FILES['duong_dan_tai_lieu'];
 
-    // Kiểm tra xem tệp ảnh đã được tải lên thành công
     if ($file['error'][0] === UPLOAD_ERR_OK) {
-        // Tạo tên tệp mới dựa trên thời gian
         $fileName = time() . '_' . $file['name'][0];
-        $filePath = $uploadDir . $fileName;
+        $filePath = $uploadDir . $fileName; // tạo đường dẫn
 
         // Kiểm tra xem tập tin đã tồn tại chưa
         if (file_exists($filePath)) {
             echo "Tập tin đã tồn tại.";
             $uploadOk = 0;
         }
-
-        // Di chuyển tệp ảnh vào thư mục lưu trữ
+        
         if (move_uploaded_file($file['tmp_name'][0], $filePath)) {
-            // Thêm thông tin và đường dẫn tệp ảnh vào CSDL
-            $sql = "INSERT INTO tbl_thuonghieu (Stt, TenGoi,UrlAnh,FileName) VALUES ('$SoThuTu', '$TenGoi', '$filePath', '$fileName')";
-            $result = $conect->query($sql);
+            $sql = "INSERT INTO tbl_thuonghieu (Stt, TenGoi, UrlAnh,FileName) VALUES (?, ?, ?,?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("ssss", $SoThuTu, $TenGoi, $filePath, $fileName);
 
-            $response = ["status" => true];
+            if ($stmt->execute()) {
+                $response = ["status" => true];
+            } else {
+                $response = ["status" => false];
+            }
         } else {
             $response = ["status" => false];
         }
     } else {
-        $response = ["status" => false];
+        $sql = "INSERT INTO tbl_thuonghieu (Stt, TenGoi) VALUES (?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ss", $SoThuTu, $TenGoi);
+
+        if ($stmt->execute()) {
+            $response = ["status" => true];
+        } else {
+            $response = ["status" => false];
+        }
     }
 
     // Trả về dữ liệu dưới dạng JSON
