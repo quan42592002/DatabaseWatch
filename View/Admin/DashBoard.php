03efe5286f90e75328ade0111c86f5fd6738b15b
quan4259 <86346279+quan4259@users.noreply.github.com>
2023-11-03 02:48:44 +0700

    th

---
+++ View/Admin/DashBoard.php
@@ -0,0 +1,116 @@
+<div id="wrapper-admin">
+    <div class="card" style="margin: 0 auto;margin-top: 20px;">
+        <div class="card-header">
+            <h3>Thông kê</h3>
+        </div>
+        <div style="width: 100%; display: flex">
+            <select name="" id="" class="form-control" style="width: 20%;">
+                <option value="">--Chọn phân loại --</option>
+                <option value="">1. Thống kê theo ngày</option>
+                <option value="">2. Thống kê theo tháng</option>
+                <option value="">2. Thống kê theo năm</option>
+            </select>
+
+            <input type="date" class="form-control" id="GiaBan"  style="width: 20%; margin-left: 10px;">
+
+
+        </div>
+        <div class="card-body p-2" style="display: flex;">
+            <div class="" style="width: 25%;">
+                <!-- small box -->
+                <div class="small-box bg-aqua">
+                    <div style="display: flex;">
+                        <div class="inner" style="width: 63%;
+                        text-align: center;
+                        margin-top: 25px;">
+                            <h3>150</h3>
+                            <p>Sản phẩm đặt mua</p>
+                        </div>
+                        <div class="icon">
+                            <i class="bi bi-bag"></i>
+                        </div>
+                    </div>
+                    <a href="#" class="small-box-footer">Xem chi tiết <i class="bi bi-arrow-right-circle"></i></a>
+                </div>
+            </div>
+
+            <div class="" style="width: 25%; margin-left: 10px;">
+                <!-- small box -->
+                <div class="small-box bg-yellow">
+                    <div style="display: flex;">
+                        <div class="inner" style="width: 63%;
+                        text-align: center;
+                        margin-top: 25px;">
+                            <h3>44</h3>
+                            <p>Tài khoản sử dụng</p>
+                        </div>
+                        <div class="icon">
+                            <i class="bi bi-person-fill-add"></i>
+                        </div>
+                    </div>
+                    <a href="#" class="small-box-footer">Xem chi tiết <i class="bi bi-arrow-right-circle"></i></a>
+                </div>
+            </div>
+
+            <div class="" style="width: 25%; margin-left: 10px;">
+                <!-- small box -->
+                <div class="small-box bg-green">
+                    <div style="display: flex;">
+                        <div class="inner" style="width: 63%;
+                        text-align: center;
+                        margin-top: 25px;">
+                            <h3>44</h3>
+                            <p>Tài khoản sử dụng</p>
+                        </div>
+                        <div class="icon">
+                            <i class="bi bi-person-fill-add"></i>
+                        </div>
+                    </div>
+                    <a href="#" class="small-box-footer">Xem chi tiết <i class="bi bi-arrow-right-circle"></i></a>
+                </div>
+            </div>
+
+            <div class="" style="width: 25%; margin-left: 10px;">
+                <!-- small box -->
+                <div class="small-box bg-red">
+                    <div style="display: flex;">
+                        <div class="inner" style="width: 63%;
+                        text-align: center;
+                        margin-top: 25px;">
+                            <h3>44</h3>
+                            <p>Tài khoản sử dụng</p>
+                        </div>
+                        <div class="icon">
+                            <i class="bi bi-person-fill-add"></i>
+                        </div>
+                    </div>
+                    <a href="#" class="small-box-footer">Xem chi tiết <i class="bi bi-arrow-right-circle"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <h5>Sản phẩm bán chạy </h5>
+        <div class="form-group" style="width: 100%; display: flex;">
+            <div class="" style="width: 100%;">
+                <table class="styled-table">
+                    <tr>
+                        <th style="text-align: center;">Imeil</th>
+                        <th style="text-align: center;">Bảo hành</th>
+                        <th style="text-align: center;">Ngày bảo hành từ</th>
+                        <th style="text-align: center;">Đến ngày</th>
+                    </tr>
+                    <tbody id="tbl_ChiTietDongHo">
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<link rel="stylesheet" href="http://localhost:3000/Assets/admin/css/dashboard.css">
