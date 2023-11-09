57d083ea9c0f0d290a67d75027b9a62b33711306
quan4259 <86346279+quan4259@users.noreply.github.com>
2023-11-09 14:18:14 +0700

    th

--- View/Admin/TaiKhoan/SuaTaiKhoan.php
+++ View/Admin/TaiKhoan/SuaTaiKhoan.php
@@ -7,7 +7,7 @@
     }
 </style>
 <?php
- if (isset($_GET['exit'])) {
+if (isset($_GET['exit'])) {
     if (isset($data['item_user'])) {
         $item_user = $data['item_user'];
         $TenDayDu = $item_user['TenDayDu'];
@@ -16,7 +16,7 @@
         $DiaChi = $item_user['DiaChi'];
         $SoDienThoai = $item_user['SoDienThoai'];
         $LoaiTaiKhoan = $item_user['LoaiTaiKhoan'];
-    }else{
+    } else {
         $TenDayDu = "";
         $Username = "";
         $Email = "";
@@ -24,9 +24,7 @@
         $SoDienThoai = "";
         $LoaiTaiKhoan = "";
     }
-    }
-
-else{
+} else {
     if (isset($data['item_user'])) {
         $item_user = $data['item_user'];
         $TenDayDu = $item_user['TenDayDu'];
@@ -36,7 +34,7 @@ else{
         $SoDienThoai = $item_user['SoDienThoai'];
         $LoaiTaiKhoan = $item_user['LoaiTaiKhoan'];
         $id = $item_user['IdUsers'];
-    }else{
+    } else {
         $TenDayDu = "";
         $Username = "";
         $Email = "";
@@ -46,7 +44,7 @@ else{
         $id = 0;
     }
 }
- 
+
 ?>
 
 <div id="wrapper-admin">
@@ -57,23 +55,23 @@ else{
         <div class="" style="width: 100%; display: flex; border-bottom: 1px solid blue; ">
             <a class="btn btn-success" href="?controller=TaiKhoanController" style="display: flex;margin: 10px; ">Quay
                 lại</a>
-   
+
         </div>
         <h3 class="error-message text-center">
             <?php
-                if (isset($_GET['exit'])) {
-                        echo urldecode($_GET['exit']);
-                    }
-                ?>
+            if (isset($_GET['exit'])) {
+                echo urldecode($_GET['exit']);
+            }
+            ?>
         </h3>
-        <div class="card-body p-2">
+        <div class="card-body p-2" style="padding-top: 0;">
             <form method="post" action="http://localhost:3000/Controller/admin/Crud/TaiKhoan/SuaTaiKhoan.php" onsubmit="return validateForm(event)">
-                <input type="id" value="<?php echo  $id?>" name="id_user" hidden>
-                <label for="FullName" class="input-label">
+                <input type="id" value="<?php echo  $id ?>" name="id_user" hidden>
+                <label for="FullName" class="input-label" >
                     Họ và tên
                 </label>
                 <input type="text" class="form-control form-control-lg" name="ten_day_du" id="FullName" value="<?php echo $TenDayDu; ?>" required>
-                <label for="user_name" class="input-label">
+                <label for="user_name" class="input-label" >
                     Tên đăng nhập
                 </label>
                 <input type="text" class="form-control form-control-lg" name="user_name" id="UserName" required value="<?php echo $Username; ?>">
@@ -82,16 +80,14 @@ else{
                     Mật khẩu mới
                 </label>
                 <div style="position: relative;">
-                    <input type="password" class="form-control form-control-lg" name="mat_khau" id="Password" 
-                        autocomplete="off" placeholder="Nếu không đổi mật khẩu thì không cần nhập trường này">
+                    <input type="password" class="form-control form-control-lg" name="mat_khau" id="Password" autocomplete="off" placeholder="Nếu không đổi mật khẩu thì không cần nhập trường này">
                 </div>
                 <p class="error-message" id="passwordError"></p>
                 <label for="mat_khau" class="input-label">
                     Xác nhận mật khẩu mới
                 </label>
                 <div style="position: relative;">
-                    <input type="password" class="form-control form-control-lg" name="mat_khau" id="ComfirmPassword" 
-                        autocomplete="off" placeholder="Nếu không đổi mật khẩu thì không cần nhập trường này">
+                    <input type="password" class="form-control form-control-lg" name="mat_khau" id="ComfirmPassword" autocomplete="off" placeholder="Nếu không đổi mật khẩu thì không cần nhập trường này">
                 </div>
                 <p class="error-message" id="passwordError"></p>
 
@@ -103,7 +99,7 @@ else{
                 <label for="so_dien_thoai" class="input-label">
                     Số điện thoại
                 </label>
-                <input type="text" class="form-control form-control-lg" name="so_dien_thoai" id="PhoneNumber"  value="<?php echo $SoDienThoai; ?>">
+                <input type="text" class="form-control form-control-lg" name="so_dien_thoai" id="PhoneNumber" value="<?php echo $SoDienThoai; ?>">
 
                 <label for="dia_chi" class="input-label">
                     Địa chỉ
@@ -113,12 +109,12 @@ else{
                 <label for="loai_tai_khoan" class="input-label">
                     Loại tài khoản
                 </label>
-                <select name="loai_tai_khoan" class="form-control form-control-lg" >
-                  
-                    <option <?php echo $LoaiTaiKhoan == 0 ? "selected" : "" ;?>  value="0">Admin</option>
-                    <option <?php echo $LoaiTaiKhoan == 1 ? "selected" : "" ;?> value="1">Khách hàng</option>
+                <select name="loai_tai_khoan" class="form-control form-control-lg">
+
+                    <option <?php echo $LoaiTaiKhoan == 0 ? "selected" : ""; ?> value="0">Admin</option>
+                    <option <?php echo $LoaiTaiKhoan == 1 ? "selected" : ""; ?> value="1">Khách hàng</option>
                 </select>
-                <button  type="submit" >Cập nhật</button>
+                <button type="submit">Cập nhật</button>
 
             </form>
         </div>
