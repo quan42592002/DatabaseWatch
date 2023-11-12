0c3a7dcfa8a88699291a622957cde0d5508a7547
vietanhpham2811 <141823836+vietanhpham2811@users.noreply.github.com>
2023-11-12 12:22:25 +0700

    th

--- Controller/admin/Crud/BaiViet/UpdateBaiViet.php
+++ Controller/admin/Crud/BaiViet/UpdateBaiViet.php
@@ -10,10 +10,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Lấy dữ liệu từ form
     $content = $_POST['content'];
     $TenBaiViet = $_POST['TenBaiViet'];
+    $IdBaiViet = $_POST['IdBaiViet'];
 
-    $sql = "INSERT INTO tbl_baiviet (TenBaiViet , NoiDung ) VALUES ('$TenBaiViet','$content')";
-    $stmt = $conn->query($sql);
 
+    $sql = "UPDATE tbl_baiviet SET TenBaiViet = ?, NoiDung = ? WHERE tbl_baiviet.IdBaiViet = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssi", $TenBaiViet, $content, $IdBaiViet);
+    $stmt->execute();
     if ($stmt == true) {
         $response = ["status" => true];
         header('Content-Type: application/json');
