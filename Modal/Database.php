b23271139ef60d870a19dcc7abb5fbcd14b21381
longdb2002 <147981895+longdb2002@users.noreply.github.com>
2023-11-12 12:00:13 +0700

    th

--- Modal/Database.php
+++ Modal/Database.php
@@ -7,7 +7,7 @@ class Database
     private $dbName = 'db_dongho';
     private $user = 'root';
     private $port = 3306;
-    private $password = 'Buimaianh4259';
+    private $password = '1142002';
 
     // Hàm kết nối CSDL
     public function connect()
